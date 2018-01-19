747698aaeab6a408656d955954e2f2987dc2dc74
frodinm <fabrizio_1197@hotmail.com>
2018-01-19 18:43:56 -0500

    Successful setup with Reactjs

--- webpack.mix.js
+++ webpack.mix.js
@@ -10,6 +10,24 @@ let mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
+mix.webpackConfig({
+    module: {
+      rules: [
+        {
+            test: /\.jsx?$/,
+            exclude: /(node_modules|bower_components)/,
+            loader: 'babel-loader',
+            query: {
+                presets: [
+                    'env',
+                    'react',
+                    'stage-3'
+                ]
+            }
+        }
+      ]
+    }
+  })
 
-mix.js('resources/assets/js/app.js', 'public/js')
+mix.react('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
