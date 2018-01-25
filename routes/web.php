add55b56aaa1851ebd7bbbfcd0c432d8ee96531b
frodinm <fabrizio_1197@hotmail.com>
2018-01-25 14:08:40 -0500

    fix merge issues

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -18,7 +19,16 @@ Route::get('/index', function () {
     return view('index');
 });
 
+Route::get('/philippe', function () {
+    return view('philippe');
+});
+
+Route::get('/xing',function(){
+	return view('xing');
+});
+
 Auth::routes();
+
 
 Route::get('/home', 'HomeController@index')->name('home');
 
