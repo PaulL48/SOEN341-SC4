54cf61af71ea0e6549d9877a3011bebbd5286ca8
nirmal864 <nirmal_864@hotmail.com>
2018-02-13 00:11:24 -0500

    Add files via upload

---
+++ VotesController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+//use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Response;
+
+use App\Vote;
+
+class VotesController extends Controller
+{
+    public function vote_question()
+    {
+        return Response::json(Vote::vote(Auth::id(), Request::get('question_id'), Request::get('vote'), 'question_id'));
+    }
+    public function vote_answer()
+    {
+        return Response::json(Vote::vote(Auth::id(), Request::get('answer_id'), Request::get('vote'), 'answer_id'));
+    }
+}
