c0305490504435fe44cd005a3569fd226ab763dc
Rahimuz Zaman <rahimuz_z@yahoo.ca>
2018-03-23 13:04:01 -0400

    add react test
    handle submit

--- resources/assets/js/__tests__/screens/AskQuestion.test.js
+++ resources/assets/js/__tests__/screens/AskQuestion.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {AskQuestion} from '../../src/screens/Questions/screens/AskQuestion.screen';
+import {AskQuestion, handleSubmit} from '../../src/screens/Questions/screens/AskQuestion.screen';
 import {shallow} from 'enzyme';
 import { expect } from 'chai';
-
+import MockAdapter from 'axios-mock-adapter';
+import Axios from 'axios';
 
 describe('AskQuestion item', () => {
       let router = {
@@ -15,7 +16,39 @@ describe('AskQuestion item', () => {
             push:()=>{}
         }
         let currentUser = {user:{name:''}}
-      const wrapper = shallow(<AskQuestion history={router} currentUser={currentUser} />);  
+        let state = {
+            title:'',
+            question: '',
+            resolved:false
+        }
+      const wrapper = shallow(<AskQuestion history={router} currentUser={currentUser} />).setState(state);  
       it('should be a div item', () => { 
              expect(wrapper.type()).to.eql('div');  });
 });
+
+/*
+describe('handleSubmit', ()=>{
+    it('handles the submission of question data', done => {
+        var mock = new MockAdapter(Axios);
+        const data= {
+            title: '',
+            question: '',
+            resolved: false
+        };
+        mock.onGet('/ask').reply(200, data);
+
+        Axios.request({
+            url:'/ask',
+            method:'post',
+            data
+        }).then((res)=>{
+            console.log(res);
+            this.props.history.push("/");
+        }).catch((err)=>{
+            console.log(err);
+        });
+      }
+    )
+
+})
+*/
